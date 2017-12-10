5fc9d3a852725b9bab0cd6e6fde39cb128f8d784
noone23 <d.schwarz25@gmail.com>
2017-12-10 17:57:56 +0000

    Empty project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spectrometerApp
+ * @description
+ * # spectrometerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spectrometerApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spectrometerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spectrometerApp
+ */
+angular.module('spectrometerApp')
+  .controller('MainCtrl', function () {
+
+  });
