7ccb0f5b766beed7e6980f45aa48f5835e42559e
noone23 <d.schwarz25@gmail.com>
2017-12-16 21:37:32 +0000

    Added wavelengths

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,20 @@ angular.module('spectrometerApp')
   .controller('MainCtrl', ['$scope', 'spectrData', function ($scope, spectrdata) {
 
     var vm = this;
-    $scope.data = spectrdata.result;
 
+    $scope.channels = {
+      'R' : 610,
+      'S' : 680,
+      'T' : 730,
+      'U' : 760,
+      'V' : 810,
+      'W' : 860
+    };
+
+    $scope.data = spectrdata.result;
     this.submit = function(d){
       spectrdata.send(d);
     };
 
-    this.getData = function(){
-      return spectrdata.result;
-    };
+
 }]);
--- app/scripts/directives/barschart.js
+++ app/scripts/directives/barschart.js
@@ -12,26 +12,20 @@ angular.module('spectrometerApp')
       restrict: 'E',
       replace: true,
       link: function (scope, element, attrs) {
-        window.onresize = function() {
-           scope.$apply();
-          };
+          window.onresize = function() {
+             scope.$apply();
+            };
 
-           //converting all data passed thru into an array
-           console.log(element);
-           //element[0] appends to the <bar-chart> tag
            var chart = d3.select(element[0])
            var parentheight = element[0].parentElement.clientHeight;
            var parentwidth = element[0].parentElement.clientWidth;
-           console.log(parentheight);
            var svg = chart.append("svg"),
               margin = {top: 20, right: 20, bottom: 30, left: 40},
-              //width = +svg.attr("width") - margin.left - margin.right,
-              //height = +svg.attr("height") - margin.top - margin.bottom;
               width = parentwidth-margin.left-margin.right,
               height = parentheight-margin.top-margin.bottom;
 
             svg.style('width', '100%').style('height', '100%');
-            var x = d3.scaleBand()
+            var x = d3.scaleLinear()
                   .range([0, width]);
             var y = d3.scaleLinear()
                   .range([height, 0]).domain([0, 500]);
@@ -49,22 +43,21 @@ angular.module('spectrometerApp')
 
             var bars = g.selectAll(".bar");
             var initialized = false;
-
+            var scalestep = 100;
 
             scope.$watch(attrs.data, function(newdata) {
-             console.log(newdata);
              if (newdata){
              var data = [];
               Object.keys(newdata).forEach(function(key){
               if (key.length<2){
-                data.push({channel : key, signal : +newdata[key]});
+                data.push({channel : scope.channels[key], signal : +newdata[key]});
               }
               });
 
-              console.log(data);
 
-              x.domain(data.map(function(d) { return d.channel; }));
-              y.domain([0, Math.ceil(d3.max(data, function(d) { return d.signal+1; })/200)*200]);
+              x.domain([d3.min(data, function(d) { return d.channel;})-40,
+                        d3.max(data, function(d) { return d.channel;})+40]);
+              y.domain([0, Math.ceil(d3.max(data, function(d) { return d.signal+1; })/scalestep)*scalestep]);
               x_axis.transition().duration(300).call(d3.axisBottom(x));
               y_axis.transition().duration(300).call(d3.axisLeft(y).ticks(4));
 
@@ -76,32 +69,12 @@ angular.module('spectrometerApp')
                   .attr("height", height - y(0));
               bars.transition().duration(300)
                 .attr("x", function(d) {return x(d.channel);})
-                .attr("width", x.bandwidth())
+                .attr("width", x(680) - x(660))
                 .attr("y", function(d) { return y(d.signal);})
                 .attr("height", function(d) { return height - y(d.signal);});
 
               }
             }, true);
-           // var data = scope.$parent.data;
-           //
-           // chart.append('text').text("Test "+ scope.data.tempC);
-           // //in D3, any selection[0] contains the group
-           // //selection[0][0] is the DOM node
-           // //but we won't need that this time
-           //
-           // //to our original directive markup bars-chart
-           // //we add a div with out chart stling and bind each
-           // //data entry to the chart
-           //  chart.append("div").attr("class", "chart")
-           //   .selectAll('div')
-           //   .data(data).enter().append("div")
-           //   .style('background-color', 'lightblue')
-           //   .style('font-color', 'white')
-           //   .style("width", function(d) { return d + "%"; })
-           //   .text(function(d) { return d + "%"; });
-           // //a little of magic: setting it's width based
-           // //on the data value (d)
-           // //and text all with a smooth transition
          }
     };
   });
